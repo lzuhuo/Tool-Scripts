c7133b884ef9d8a7adc6ad1a8fba9bf3c6b6e015
Elizeu Oliveira <homenzinhoptr@gmail.com>
2024-09-12 17:44:07 -0300

    Update programs.sh

--- programs.sh
+++ programs.sh
@@ -14,17 +14,17 @@ cd ..
 #Download de Programas
 mkdir programas
 cd programas
-wget https://www.microsip.org/download/MicroSIP-Lite-3.21.3.exe
+wget https://www.microsip.org/download/MicroSIP-Lite-3.21.4.exe
 wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_120.0.2210.133-1_amd64.deb
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_128.0.2739.79-1_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://cdn.zoom.us/prod/5.17.1.1840/zoom_amd64.deb
-wget https://sqlopsbuilds.azureedge.net/stable/b6f7beb01f92adaa4b79b6b6f3ac704e95cafe6e/azuredatastudio-linux-1.47.1.deb
-wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/code_1.85.1-1702462158_amd64.deb
-wget https://download.dbeaver.com/community/23.3.2/dbeaver-ce_23.3.2_amd64.deb
-wget https://download.anydesk.com/linux/anydesk_6.3.0-1_amd64.deb
-wget https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-7.9.0-Linux-x64.deb
-wget https://github.com/ferdium/ferdium-app/releases/download/v6.7.0/Ferdium-linux-6.7.0-amd64.deb
+wget https://zoom.us/client/6.1.11.1545/zoom_amd64.deb
+wget https://download.microsoft.com/download/7/8/3/783c2037-8607-43c4-a593-0936e965d38b/azuredatastudio-linux-1.49.1.deb
+wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/38c31bc77e0dd6ae88a4e9cc93428cc27a56ba40/code_1.93.1-1726079302_amd64.deb
+wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+wget https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb
+wget https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-7.12.1-Linux-x64.deb
+wget https://github.com/ferdium/ferdium-app/releases/download/v6.7.6/Ferdium-linux-6.7.6-amd64.deb
 
 #Instalação de Programas baixados e reparos de instalação
 sudo dpkg -i *.deb
