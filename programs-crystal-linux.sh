c19e34001e2932495ee66bd1882da8fc80db4c30
Elizeu Oliveira <homenzinhoptr@gmail.com>
2024-11-12 10:44:00 -0300

    Add files via upload

---
+++ programs-crystal-linux.sh
@@ -0,0 +1,24 @@
+#Edit Mirror List
+sudo nano /etc/pacman.conf
+#Uncomment lines above without #
+#[multilib]
+#Include = /etc/pacman.d/mirrorlist
+
+#Atualização de Pacotes
+sudo pacman -Syu
+
+#Instalação de Programas via terminal
+sudo pacman -S tilix flameshot gnome-tweaks gnome-browser-connector firefox inetutils
+
+#Download de Programas Padrão
+ame -S google-chrome microsoft-edge-stable-bin zoom --noconfirm
+
+#Download de Programas Desenv
+ame -S visual-studio-code-bin azuredatastudio-bin dbeaver-ce-bin postman-bin --noconfirm
+
+#Download de Programas Infra
+ame -S freedownloadmanager realvnc-vnc-viewer ferdium-bin winbox3 inetutils --noconfirm
+
+#Download de Programas Utilitarios
+ame -S wine-mono microsip realvnc-vnc-viewer gtkglext anydesk-bin --noconfirm
+
