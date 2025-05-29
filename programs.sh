df328c2ab58301d8962f1926f04d5d360fcfbe09
Elizeu Eslon Marinho Oliveira <elizeu@aslanet.com.br>
2025-05-29 12:09:33 -0300

    Update softwares sources | Add new apps

--- programs.sh
+++ programs.sh
@@ -4,6 +4,9 @@ sudo apt upgrade -y
 
 #Instalação de Programas via terminal
 sudo apt install tilix flameshot gnome-tweaks wireshark chrome-gnome-shell thunderbird inetutils-*  -y
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #Instalação do Winbox
 git clone https://github.com/lzuhuo/winbox-installer.git
@@ -14,17 +17,17 @@ cd ..
 #Download de Programas
 mkdir programas
 cd programas
-wget https://www.microsip.org/download/MicroSIP-Lite-3.21.4.exe
+wget https://www.zoiper.com/en/voip-softphone/download/zoiper5/for/linux-deb
 wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_128.0.2739.79-1_amd64.deb
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_136.0.3240.92-1_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://zoom.us/client/6.1.11.1545/zoom_amd64.deb
-wget https://download.microsoft.com/download/7/8/3/783c2037-8607-43c4-a593-0936e965d38b/azuredatastudio-linux-1.49.1.deb
-wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/38c31bc77e0dd6ae88a4e9cc93428cc27a56ba40/code_1.93.1-1726079302_amd64.deb
+wget https://cdn.zoom.us/prod/6.4.12.2180/zoom_amd64.deb
+wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/848b80aeb52026648a8ff9f7c45a9b0a80641e2e/code_1.100.2-1747260578_amd64.deb
 wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-wget https://download.anydesk.com/linux/anydesk_6.3.2-1_amd64.deb
-wget https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-7.12.1-Linux-x64.deb
-wget https://github.com/ferdium/ferdium-app/releases/download/v6.7.6/Ferdium-linux-6.7.6-amd64.deb
+wget https://download.anydesk.com/linux/anydesk_7.0.0-1_amd64.deb
+wget https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-7.13.1-Linux-x64.deb
+wget https://github.com/ferdium/ferdium-app/releases/download/v7.1.0/Ferdium-linux-7.1.0-amd64.deb
+wget https://github.com/Ulauncher/Ulauncher/releases/download/5.15.7/ulauncher_5.15.7_all.deb
 
 #Instalação de Programas baixados e reparos de instalação
 sudo dpkg -i *.deb
@@ -34,6 +37,10 @@ cd ..
 
 #Instalação de aplicativos Snap
 snap install postman
+
+#Instalação de aplicativos Flatpak
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak run com.mattjakeman.ExtensionManager
 
 sudo rm -r programas
 sudo rm -r winbox-installer
