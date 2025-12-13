1ee90d4935a21ceacbd21b35144326f927d349b4
Elizeu Oliveira <homenzinhoptr@gmail.com>
2025-12-13 11:11:24 -0300

    Replace 'ame' with 'yay' for AUR installations

--- programs-arch-linux.sh
+++ programs-arch-linux.sh
@@ -3,37 +3,36 @@
 # Atualização de Pacotes
 sudo pacman -Syu
 
-# Instalação de Programas via terminal
-sudo pacman -S git curl wget flameshot gnome-tweaks gnome-browser-connector firefox inetutils
+# Instalação de Programas via terminal (repositórios oficiais)
+sudo pacman -S --noconfirm git curl wget flameshot gnome-tweaks gnome-browser-connector firefox inetutils
 
 # Instalação do PipeWire
-sudo pacman -S pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
+sudo pacman -S --noconfirm pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
 systemctl --user enable pipewire pipewire-pulse wireplumber
 systemctl --user start pipewire pipewire-pulse wireplumber
 
 # Instalação de Programas para OpenVPN
-sudo pacman -S openvpn networkmanager-openvpn network-manager-applet
+sudo pacman -S --noconfirm openvpn networkmanager-openvpn network-manager-applet
 
 # Instalação de Requisitos
-sudo pacman -S ttf-liberation xcb-util-cursor qt5-remoteobjects qt5-webengine
+sudo pacman -S --noconfirm ttf-liberation xcb-util-cursor qt5-remoteobjects qt5-webengine
 
-# Instalação do ame - Amethyst
-sudo pacman -S --needed base-devel pacman-contrib cargo
-git clone https://gitlab.com/crystal-linux/pkgbuilds/ame.git
-cd ame
-makepkg -si
+# Instalação do yay (AUR helper)
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
 cd ..
 
-# Instalação de extensões GNOME
-ame -S \
+# Instalação de extensões GNOME (via AUR)
+yay -S --noconfirm \
   gnome-shell-extension-all-windows-srwp \
   gnome-shell-extension-blur-my-shell \
   gnome-shell-extension-magic-lamp \
   gnome-shell-extension-compiz-windows-effect \
   gnome-shell-extension-dash-to-dock \
   gnome-shell-extension-show-desktop-button \
-  gnome-shell-extension-vitals \
-  --noconfirm
+  gnome-shell-extension-vitals
 
 # Ativação das extensões GNOME
 gnome-extensions enable all-windows-srwp@jakavery.github.io
@@ -44,11 +43,11 @@ gnome-extensions enable dash-to-dock@micxgx.gmail.com
 gnome-extensions enable show-desktop-button@amivaleo
 gnome-extensions enable Vitals@CoreCoding.com
 
-# Download de Programas Padrão
-ame -S google-chrome microsoft-edge-stable-bin zoom --noconfirm
+# Download de Programas Padrão (AUR)
+yay -S --noconfirm google-chrome microsoft-edge-stable-bin zoom
 
-# Download de Programas de Desenvolvimento
-ame -S visual-studio-code-bin azuredatastudio-bin dbeaver-ce-bin postman-bin --noconfirm
+# Download de Programas de Desenvolvimento (AUR)
+yay -S --noconfirm visual-studio-code-bin azuredatastudio-bin dbeaver-ce-bin postman-bin
 
 # Instalação do Node Version Manager (NVM)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
@@ -56,12 +55,11 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 
-# Download de Programas de Infraestrutura
-ame -S realvnc-vnc-viewer ferdium-bin winbox inetutils extension-manager keepassxc --noconfirm
+# Download de Programas de Infraestrutura (AUR)
+yay -S --noconfirm realvnc-vnc-viewer ferdium-bin winbox inetutils extension-manager keepassxc
 
-# Download de Programas Utilitários
-ame -S wine-mono microsip realvnc-vnc-viewer gtkglext anydesk-bin ulauncher --noconfirm
+# Download de Programas Utilitários (AUR)
+yay -S --noconfirm wine-mono microsip realvnc-vnc-viewer gtkglext anydesk-bin ulauncher freedownloadmanager-bin
 
-# Download de Programas Específicos
-ame -S gtkd --noconfirm
-ame -S tilix --noconfirm
+# Download de Programas Específicos (AUR)
+yay -S --noconfirm gtkd tilix
