7196756747a1233bdd09cedd9dcfb6b8131d6189
Elizeu Oliveira <homenzinhoptr@gmail.com>
2026-02-14 10:10:43 -0300

    Update program downloads and Flatpak installations
    Added new downloads for Azure Data Studio and Wifiman, and updated Flatpak installations for Postman, WinBox, KeePassXC, Zoom, Chrome, Free Download Manager, Edge, DBeaver, AnyDesk, Ferdium, and Visual Studio Code.

--- programs.sh
+++ programs.sh
@@ -12,15 +12,9 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 mkdir programas
 cd programas
 wget https://www.zoiper.com/en/voip-softphone/download/zoiper5/for/linux-deb
-wget https://files2.freedownloadmanager.org/6/latest/freedownloadmanager.deb
-wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_145.0.3800.58-1_amd64.deb
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://cdn.zoom.us/prod/6.7.5.6891/zoom_amd64.deb
-wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/b6a47e94e326b5c209d118cf0f994d6065585705/code_1.109.3-1770920364_amd64.deb
-wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
-wget https://download.anydesk.com/linux/anydesk_7.1.4-1_amd64.deb
 wget https://downloads.realvnc.com/download/file/viewer.files/VNC-Viewer-7.15.1-Linux-x64.deb
-wget https://github.com/ferdium/ferdium-app/releases/download/v7.1.1/Ferdium-linux-7.1.1-amd64.deb
+wget https://download.microsoft.com/download/6b2bfeac-9c1b-4182-9a2f-ce86ff8cc371/azuredatastudio-linux-1.52.0.deb
+wget https://desktop.wifiman.com/wifiman-desktop-1.2.8-amd64.deb
 
 #Instalação de Programas baixados e reparos de instalação
 sudo dpkg -i *.deb
@@ -28,9 +22,18 @@ sudo apt install -fy
 cd ..
 
 #Instalação de aplicativos Flatpak
-flatpak run com.mattjakeman.ExtensionManager
 flatpak install flathub com.mattjakeman.ExtensionManager
-flatpak run com.mattjakeman.ExtensionManager
+flatpak install flathub com.getpostman.Postman
+flatpak install flathub com.mikrotik.WinBox
+flatpak install flathub org.keepassxc.KeePassXC
+flatpak install flathub us.zoom.Zoom
+flatpak install flathub com.google.Chrome
+flatpak install flathub org.freedownloadmanager.Manager
+flatpak install flathub com.microsoft.Edge
+flatpak install flathub io.dbeaver.DBeaverCommunity
+flatpak install flathub com.anydesk.Anydesk
+flatpak install flathub org.ferdium.Ferdium
+flatpak install flathub com.visualstudio.code
 
 sudo rm -r programas
 sudo apt update
